aa905cf43f068412eb7e8aa171b6061af62e4cce
jrazafim <tanel06@yahoo.fr>
2018-08-01 17:11:55 +0200

    Ajout du fichier planning_Nat.py

---
+++ planning_Nat.py
@@ -0,0 +1,98 @@
+Planning = {'Lundi' : 9,
+            'Mardi' : 8,
+            'Mercredi' : 3,
+            'Jeudi' : 8,
+            'Vendredi' : 8}
+
+Semaine_16_04 = {'Lundi' : "11h30 - 17h30",
+            'Mardi' : "10h - 17h30",
+            'Mercredi' : "10h - 17h30",
+            'Jeudi' : "10h - 17h30",
+            'Vendredi' : "10h - 17h30"}
+
+Semaine_23_04 = {'Lundi' : ["09h30 - 17h30", ("Sonya", "Tan")],
+            'Mardi' : "tsy mandeha",
+            'Mercredi' : ["9h30 - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h30", ("Tan", "Sonya")],
+            'Vendredi' : ["10h - 17h30", ("Sonya", "Tan")]}
+
+Semaine_30_04 = {'Lundi' : [" 10h - 17h30", ("Tan", "So")],
+            'Mardi' : "tsy mandeha",
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h30", ("So", "Tan")],
+            'Vendredi' : ["10h - 17h30", ("Tan", "Tan")]}
+
+Semaine_07_05 = {'Lundi' : [" 10h - 17h30", ("Tan", "So")],
+            'Mardi' : "tsy mandeha",
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : "tsy mandeha",
+            'Vendredi' : ["10h - 17h30", ("Tan", "Tan")]}
+
+Semaine_14_05 = {'Lundi' : [" 9h30 - 17h30", ("Tan", "Tan")],
+            'Mardi' : [" 10h - 17h", ("So", "So")],
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h30", ("Tan", "so")],
+            'Vendredi' : ["11h30 - 17h30", ("So", "Tan")]}
+
+Semaine_21_05 = {'Lundi' : "Tsy mandeha",
+            'Mardi' : [" 10h - 17h30", ("Tan", "Tan")],
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["11h - 17h30", ("So", "Tan")],
+            'Vendredi' : ["11 - 17h30", ("So", "Tan")]}
+            
+Semaine_28_05 = {'Lundi' : [" 11h - 17h30", ("Tan", "so")],
+            'Mardi' : [" 10h - 19h15", ("so", "so")],
+            'Mercredi' : ["8h30 - 18h15", ("so", "so")],
+            'Jeudi' : ["8h30 - 19h30", ("So", "so")],
+            'Vendredi' :"Tsy mandeha"}
+
+            
+Semaine_04_06 = {'Lundi' : [" 9h30 - 17h30", ("So", "Tan")],
+            'Mardi' : [" 10h - 14h", ("so", "so")],
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Vendredi' :["10h - 17h30", ("So", "Tan")]}
+
+Semaine_11_06 = {'Lundi' : ["9h30 - 17h30", ("So", "Tan")],
+            'Mardi' : ["10h - 17h30", ("So", "Tan")],
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h30", ("Tan", "So")],
+            'Vendredi' :"Tsy mandeha"}
+
+Semaine_18_06 = {'Lundi' : ["10h - 17h30", ("So", "Tan")],
+            'Mardi' : ["13h - 17h30", ("So", "Tan")],
+            'Mercredi' : ["11h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Vendredi' : ["10h - 17h30", ("Tan", "Tan")]}
+
+Semaine_25_06 = {'Lundi' : ["10h - 16h", ("Tan", "Tan")],
+            'Mardi' : ["9h - 15h30", ("Tan", "Tan")],
+            'Mercredi' : ["10h - 17h30", ("Tan", "Tan")],
+            'Jeudi' : ["10h - 17h", ("Tan", "Tan")],
+            'Vendredi' : ["10h - 17h", ("Tan", "Tan")]}
+
+Semaine_02_07 = {'Lundi' : ["10h - 18h", ("Tan", "Tan")],
+            'Mardi' : ["10h - 18h", ("So", "Tan")],
+            'Mercredi' : ["10h - 18h", ("Tan", "So")],
+            'Jeudi' : ["10h - 18h", ("So", "Tan")],
+            'Vendredi' : "Tsy mandeha"}
+
+Semaine_09_07 = {'Lundi' : ["10h - 18h", ("Tan", "Tan")],
+            'Mardi' : ["9h - 18h", ("So", "Tan")],
+            'Mercredi' : ["10h - 18h", ("Tan", "So")],
+            'Jeudi' : ["10h - 18h", ("So", "Tan")],
+            'Vendredi' : "Tsy mandeha"}
+
+
+Semaine_16_07 = {'Lundi' : ["10h - 17h30", ("Tan", "So")],
+            'Mardi' : ["10h - 13h", ("Tan", "So")],
+            'Mercredi' : ["10h - 17h30", ("Tan", "So")],
+            'Jeudi' : ["10h - 18h", ("Tan", "Tan")],
+            'Vendredi' : ["10h - 18h", ("So", "Tan")]}
+                 
+                 
+Semaine_23_07 = {'Lundi' : ["10h - 18h", ("So", "Tan")],
+            'Mardi' : ["10h - 18h", ("Tan", "So")],
+            'Mercredi' : ["10h - 18h", ("Tan", "So")],
+            'Jeudi' : ["10h - 18h", ("Tan", "Tan")],
+            'Vendredi' : "Tsy mandeha"}
